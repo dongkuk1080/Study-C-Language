9ceb3eaf8a29c41ad79f9fc96dce9b1c384f43aa
unknown <dongkuk1080@gmail.com>
2020-07-15 00:27:20 +0900

    20200715 기초문법 끝

---
+++ Study_C/Study_C/파일입출력.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#if 0
+int main(void)
+{
+	FILE* fp = NULL;
+	fp = fopen("output.txt", "w");
+	//w -> 쓰기모드, r-> 읽기모드
+	if (fp == NULL)
+		printf("파일 열기에 실패했습니다.");
+	else
+		printf("파일 열기에 성공했습니다.");
+	fputc('H', fp);
+	fputc('E', fp);
+	fputc('L', fp);
+	fputc('L', fp);
+	fputc('O', fp);
+	fclose(fp);
+
+	return 0;
+}
+#endif
